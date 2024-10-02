250a4c50f7466caa21ac9c4369971e797cde5ca0
Alex Kadai <alexandrkadai@gmail.com>
2024-10-02 19:40:05 +0300

    Data events fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,16 @@
+import Link from 'next/link';
+
 const Homepage = () => {
   return (
-    <div className=''>Homepage</div>
-  )
-}
+    <div className="w-screen h-screen flex items-center justify-center">
+      <div className="w-[400px] h-[450px] flex flex-col justify-center items-center gap-4">
+        <Link className='uppercase bg-purple-500 rounded-md text-white p-2' href="/admin"> Admin Dashboard</Link>
+        <Link className='uppercase bg-purple-500 rounded-md text-white p-2' href="/list/teachers/1"> Teacher Dashboard</Link>
+        <Link className='uppercase bg-purple-500 rounded-md text-white p-2' href="/list/students/1"> Student Dashboard</Link>
+        <Link className='uppercase bg-purple-500 rounded-md text-white p-2' href="/list/teachers"> Teachers List</Link>
+      </div>
+    </div>
+  );
+};
 
-export default Homepage
+export default Homepage;
--- src/components/BigCalendar.tsx
+++ src/components/BigCalendar.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Calendar, momentLocalizer, View, Views } from 'react-big-calendar';
 import moment from 'moment';
-import { calendarEvents } from '@/lib/data';
+import { calendarEvents } from '../lib/data';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 
 const localizer = momentLocalizer(moment);
@@ -19,13 +19,12 @@ const BigCalendar = () => {
       events={calendarEvents}
       startAccessor="start"
       endAccessor="end"
-      defaultView={'work_week'}
-      views={['work_week', 'day']}
+      views={['work_week', 'day', 'month']}
       view={view}
       style={{ height: '98%' }}
       onView={handleOnChangeView}
-      min={new Date(2024, 1, 0, 8, 0, 0)}
-      max={new Date(2024, 1, 0, 18, 0, 0)}
+      min={new Date(2025, 1, 0, 8, 0, 0)}
+      max={new Date(2025, 1, 0, 17, 0, 0)}
     />
   );
 };
